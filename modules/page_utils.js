49e94c225023b017f2dd52e2284e3de84b7b325d
atroberg <alexistroberg@gmail.com>
2014-03-21 10:21:17 +0100

    Fix: touch feedback

--- modules/page_utils.js
+++ modules/page_utils.js
@@ -6,16 +6,36 @@ var PageUtils = {
     $('body').on('touchstart', '.touchFeedback', function(e) {
       var $el = $(this);
 
+      var btnClasses = PageUtils.getBtnClasses($el);
+
       setTimeout(function() {
-        $el.removeClass('touchdown active');
+        $el.removeClass(btnClasses.removeClass);
       }, settings.buttonTouchFeedbackTimeout)
 
-      $el.addClass('touchdown active');
+      $el.addClass(btnClasses.addClass);
     })
     .on('touchend', '.touchFeedback', function(e) {
-      $(this).removeClass('touchdown active');
+      var btnClasses = PageUtils.getBtnClasses($(this));
+      $(this).removeClass(btnClasses.removeClass);
     });
   },
+
+  getBtnClasses: function($el) {
+    var addClass = 'touchdown';
+    var removeClass = 'touchdown';
+
+    var isBootstrapBtn = $el.hasClass('btn');
+
+    if ( isBootstrapBtn ) {
+      addClass += ' active';
+      removeClass += ' active';
+    }
+
+    return {
+      addClass: addClass,
+      removeClass: removeClass,
+    };
+  }
 
 };
 
